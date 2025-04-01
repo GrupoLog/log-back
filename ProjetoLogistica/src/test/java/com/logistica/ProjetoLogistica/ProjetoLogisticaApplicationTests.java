4ca177af953aca1fb1435ccf1aca75b3fe70e966
arthurCapistrano <arthur.capistrano12@gmail.com>
2025-04-01 17:40:05 -0300

    add: init spring JBDC project

---
+++ ProjetoLogistica/src/test/java/com/logistica/ProjetoLogistica/ProjetoLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistica.ProjetoLogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
