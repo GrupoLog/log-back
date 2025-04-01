891e57714128ebefb7ad18f48c08fc439f7647c6
arthurCapistrano <arthur.capistrano12@gmail.com>
2025-04-01 19:44:49 -0300

    add: project files

---
+++ demo/bd-project/src/main/java/com/cesar/bd_project/BdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.bd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdProjectApplication.class, args);
+	}
+
+}
---
+++ demo/bd-project/src/test/java/com/cesar/bd_project/BdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.bd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
