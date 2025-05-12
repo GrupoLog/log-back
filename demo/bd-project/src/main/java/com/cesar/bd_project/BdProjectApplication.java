81a8315d0c2918c232476699e95aa43022e9e35e
Erico <erico.chen@hotmail.com>
2025-05-12 18:48:55 -0300

    [refactor]: limpa o arquivo principal da aplicacao.

--- demo/bd-project/src/main/java/com/cesar/bd_project/BdProjectApplication.java
+++ demo/bd-project/src/main/java/com/cesar/bd_project/BdProjectApplication.java
@@ -1,45 +1,12 @@
 package com.cesar.bd_project;
 
-import com.cesar.bd_project.dao.ClientDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import com.cesar.bd_project.model.ClientModel;
 
 @SpringBootApplication
-public class BdProjectApplication implements CommandLineRunner{
-
-	@Autowired
-    private ClientDao clientDao;
+public class BdProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdProjectApplication.class, args);
 	}
-
-	@Override
-    public void run(String... args) throws Exception {
-        String cpf = "12345678901";
-
-        // Verifica se o cliente já existe no banco
-        ClientModel existingClient = clientDao.findById(cpf);
-
-        if (existingClient == null) {
-            // Cliente não existe, insere no banco
-            ClientModel newClient = new ClientModel(
-                cpf,
-                "João",
-                "Silva",
-                "Rua das Flores",
-                "Centro",
-                123,
-                "Recife"
-            );
-
-            clientDao.save(newClient);
-            System.out.println("Um novo cliente foi inserido.");
-        } else {
-            System.out.println("Cliente com o CPF " + cpf + " já existe no banco.");
-        }
-    }
 }
